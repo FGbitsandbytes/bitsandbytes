22b6b11fb98ac6ffcb45b6bef86ce89fb612a1af
lbartuzifg <lbartuzi@glovia.com>
2021-07-23 14:35:32 +0200

    fixed creation of multiple buckets when restarting script

--- weather_script_nohat.py
+++ weather_script_nohat.py
@@ -10,26 +10,26 @@ from os import system, name
 api_key = "d1d94c09e3dfb8e2e3cdbc0c8ad85a00"
 city = "Eindhoven"
 url = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric" % (city, api_key)
-logger = Streamer(bucket_name="Sense Hat Sensor Data", access_key="ist_mn7UAdh6xjknCMuT6JDE2P4gCVhI72pq")
+logger = Streamer(bucket_name="Weather Data", bucket_key="d40979dc-2fe8-402c-94b9-76f0727c4c16", access_key="ist_mn7UAdh6xjknCMuT6JDE2P4gCVhI72pq")
 
 
 try:
         while True:
 		response = requests.get(url)
 		data = json.loads(response.text)
-		
+
 		main = data['main']
 		temperature = main['temp']
 		temp_feel_like = main['feels_like']
 		humidity = main['humidity']
 		pressure = main['pressure']
-                
+
 		logger.log("Temperature C: ",temperature)
 		logger.log("Feels like: ",temp_feel_like)
 		logger.log("Humidity :",humidity)
 		logger.log("Pressure:",pressure)
 
-                time.sleep(5)
+		time.sleep(5)
 except KeyboardInterrupt:
         pass
 
